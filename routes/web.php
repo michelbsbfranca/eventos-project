d621fd474c342e4fbb10606b802e8839fb43c6a3
Michel <michelbsbfranca@gmail.com>
2025-04-21 21:48:51 -0300

    update: Inserido o midlware de autenticacao a rota

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ use App\Http\Controllers\EventController;
 
 Route::get('/',[EventController::class, 'index']);
 
-Route::get('/events/create',[EventController::class, 'create']);
-Route::post('/events',[EventController::class, 'store']);
-Route::delete('/events/{id}',[EventController::class, 'delete']);
+Route::get('/events/create',[EventController::class, 'create'])->middleware('auth');
+Route::post('/events',[EventController::class, 'store'])->middleware('auth');
+Route::delete('/events/{id}',[EventController::class, 'delete'])->middleware('auth');
 Route::get('/events/{id}',[EventController::class, 'show']);
 
 Route::middleware([
